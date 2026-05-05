639cab52f42e8112ab9f88275e7c0d075b77d49d
pbeltranjuan-commits <pbeltranjuan@gmail.com>
2026-05-05 13:26:32 +0200

    Update xats.py

--- xats.py
+++ xats.py
@@ -25,7 +25,7 @@ def get_embedding(text):
 def extract_text_from_image_vision(image_bytes):
     try:
         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-2.0-flash")
         img = Image.open(io.BytesIO(image_bytes))
         response = model.generate_content([
             img,
